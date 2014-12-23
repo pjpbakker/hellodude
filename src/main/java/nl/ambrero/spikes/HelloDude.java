254f58c564b9dc9d341459c4c3da6192f909101e
usbpaul <usb.paul@gmail.com>
2014-12-23 15:49:53 +0100

    Refactoring.

--- src/main/java/nl/ambrero/spikes/HelloDude.java
+++ src/main/java/nl/ambrero/spikes/HelloDude.java
@@ -2,10 +2,6 @@ package nl.ambrero.spikes;
 
 public class HelloDude {
     public static void main(String[] args) {
-		sayHello();
-    }
-
-	public static void sayHello() {
         System.out.printf("Hello, dude");
-	}
+    }
 }
