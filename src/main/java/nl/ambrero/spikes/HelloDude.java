e76998bc7a56d9183ecfdf5beacd38ee4bc69f25
usbpaul <usb.paul@gmail.com>
2014-12-23 15:52:34 +0100

    Refactor.

--- src/main/java/nl/ambrero/spikes/HelloDude.java
+++ src/main/java/nl/ambrero/spikes/HelloDude.java
@@ -4,4 +4,8 @@ public class HelloDude {
     public static void main(String[] args) {
         System.out.printf("Hello, dude");
     }
+
+    public static String sayHello() {
+        return "Hello, dude";
+    }
 }
